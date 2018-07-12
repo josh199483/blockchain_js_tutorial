89ed38af5d248985e1207bd63443514721037196
Joshua Chen <joshua830507@gmail.com>
2018-07-13 00:15:27 +0800

    add decentralized node function

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const uuid = require('uuid/v1');
 const port = process.argv[2];
 const Blockchain = require('./blockchain');
+const rp = require('request-promise');
 
 const nodeAddress = uuid().split('-').join('');
 const bitcoin = new Blockchain();
@@ -47,18 +48,60 @@ app.get('/mine', function (req, res) {
 // register a node and broadcast it the network
 app.post('/register-and-broadcast-node', function(req, res) {
     const newNodeUrl = req.body.newNodeUrl;
+    if (bitcoin.networkNodes.indexOf(newNodeUrl) == -1) bitcoin.networkNodes.push(newNodeUrl);
+
+    const regNodesPromises = [];
+    // 這邊要把新加入 blockchain 的 node 加進 networkNodes
+    bitcoin.networkNodes.forEach(networkNodeUrl => {
+		const requestOptions = {
+			uri: networkNodeUrl + '/register-node',
+			method: 'POST',
+			body: { newNodeUrl: newNodeUrl },
+			json: true
+		};
+        // 為了要讓一個 url 能夠發 request 給另一個 url 使用異步 promise  request 的 package，回傳的都是 promise 物件
+		regNodesPromises.push(rp(requestOptions));
+    });
+    
+    // 因為是異步請求，只要其中一個完成就會進入成功的事件，不用等待其他事件
+    Promise.all(regNodesPromises)
+	.then(data => {
+		const bulkRegisterOptions = {
+			uri: newNodeUrl + '/register-nodes-bulk',
+            method: 'POST',
+            // ...是把 list 攤開
+			body: { allNetworkNodes: [ ...bitcoin.networkNodes, bitcoin.currentNodeUrl ] },
+			json: true
+		};
+
+		return rp(bulkRegisterOptions);
+	})
+	.then(data => {
+		res.json({ note: 'New node registered with network successfully.' });
+	});
 });
 
 
 // register a node with the network
 app.post('/register-node', function(req, res) {
-
+    const newNodeUrl = req.body.newNodeUrl;
+	const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(newNodeUrl) == -1;
+	const notCurrentNode = bitcoin.currentNodeUrl !== newNodeUrl;
+	if (nodeNotAlreadyPresent && notCurrentNode) bitcoin.networkNodes.push(newNodeUrl);
+	res.json({ note: 'New node registered successfully.' });
 });
 
 
 // register multiple nodes at once
 app.post('/register-nodes-bulk', function(req, res) {
+    const allNetworkNodes = req.body.allNetworkNodes;
+	allNetworkNodes.forEach(networkNodeUrl => {
+		const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(networkNodeUrl) == -1;
+		const notCurrentNode = bitcoin.currentNodeUrl !== networkNodeUrl;
+		if (nodeNotAlreadyPresent && notCurrentNode) bitcoin.networkNodes.push(networkNodeUrl);
+	});
 
+	res.json({ note: 'Bulk registration successful.' });
 });
 
  
